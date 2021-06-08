a0bc4aa06fcf53f19ef73dd0587abb04b322cc0d
weng0418 <62419535+weng0418@users.noreply.github.com>
2021-06-08 16:56:31 +0800

    Update index.js

--- JumpBird/index.js
+++ JumpBird/index.js
@@ -1,11 +1,4 @@
 let skyBg, landBg, bird, game, pipes;
-
-// 考虑到计时器的使用频率非常的高，所以专门封装一个函数
-// 该函数返回一个对象，对象包含两个属性
-// start:创建一个计时器   stop:停止计时器
-// duration:计时器每隔多少毫秒执行
-// callback:每隔duration毫秒执行callback函数
-// thisObj:为了得到正确的this指向
 let getTimer = function (duration, thisObj, callback) {
   var timer = null;
   return {
@@ -25,10 +18,10 @@ let getTimer = function (duration, thisObj, callback) {
   };
 }
 
-// 系统对象，统一管理其他对象的开始和结束
+// 系統對象，統一管理其他對象的開始和結束
 game = {
-  paused : true,  // 当前游戏是否暂停
-  isGmaeOver : false, // 当前游戏是否结束
+  paused : true,  // 當前遊戲是否暫停
+  isGmaeOver : false, // 當前遊戲是否結束
   dom : document.querySelector('#game'),
   start : function(){
     skyBg.timer.start();
@@ -46,30 +39,30 @@ game = {
     pipes.produceTimer.stop();
     pipes.moveTimer.stop();
   },
-  // 该方法用于判断游戏是否结束
+    // 該方法用於判斷遊戲是否結束
   gameOver : function(){
-    // 游戏有两种情况都会导致游戏结束：1.  小鸟落地  2. 小鸟碰撞柱子
-    // 1. 小鸟落地
+    // 遊戲有兩種情況都會導致遊戲結束：1. 小鳥落地 2. 小鳥碰撞柱子
+    // 1. 小鳥落地
     if (bird.top === 462){
-      alert('游戏结束');
+      alert('遊戲結束');
 
       this.isGmaeOver = true;
       this.stop();
     }
-    // 2. 小鸟是否碰撞到柱子（需要检测碰撞）
-    // 小鸟中心点
+    // 2. 小鳥是否碰撞到柱子（需要檢測碰撞）
+    // 小鳥中心點
     let bx = bird.left + (bird.width/2);
     let by = bird.top + (bird.height/2);
-    // 柱子中心点
+    // 柱子中心點
     for(let i=0;i<pipes.all.length;i++){
-      let p = pipes.all[i]; // 当前的柱子
-      // 获取当前柱子的中心点
+      let p = pipes.all[i]; // 當前的柱子
+      // 獲取當前柱子的中心點
       let px = p.left + (p.width/2);
       let py = p.top + (p.height/2);
       // 判断是否碰撞
       if (Math.abs(bx - px) < (p.width + bird.width) / 2 && 
         Math.abs(by - py) < (p.height + bird.height) / 2){
-        alter('游戏结束');
+        alter('遊戲結束');
         this.isGmaeOver = true;
         this.stop();
       }
@@ -77,11 +70,11 @@ game = {
   }
 }
 
-// 天空对象
+// 天空對象
 skyBg = {
   left: 0,
   dom: document.querySelector('#game .sky'),
-  // 该方法用于重新更新天空的 left 值
+  // 該方法用於重新更新天空的 left 值
   show: function () {
     this.dom.style.left = this.left + 'px'
   }
@@ -94,7 +87,7 @@ skyBg.timer = getTimer(30, skyBg, function () {
   this.show();
 })
 
-// 大地对象
+// 大地對象
 landBg = {
   left: 0,
   dom: document.querySelector('#game .land'),
@@ -110,20 +103,20 @@ landBg.timer = getTimer(30, landBg, function () {
   this.show();
 })
 
-// 小鸟对象
+// 小鳥對象
 bird = {
   width : 33,
   height : 26,
   top : 150,
   left : 200,
   dom : document.querySelector('#game .bird'),
-  wingIndex : 0, // 该属性用于记录当前小鸟的背景图片
-  speed : 0, // 小鸟往下面掉的速度
+  wingIndex : 0, // 該屬性用於記錄當前小鳥的背景圖片
+  speed : 0, // 小鳥往下面掉的速度
   a : 0.0005, // 加速度
-  // 这个是关键参数
-  // 显示小鸟的方法：统一在 show 方法中显示小鸟的最终状态
+  // 這個是關鍵參數
+  // 顯示小鳥的方法：統一在 show 方法中顯示小鳥的最終狀態
   show : function(){
-    // 根据图片的索引，来设置当前小鸟背景图的位置
+    // 根據圖片的索引，來設置當前小鳥背景圖的位置
     if (this.wingIndex === 0){
       this.dom.style.backgroundPosition = '-8px -10px';
     } else if (this.wingIndex === 1){
@@ -131,10 +124,10 @@ bird = {
     } else {
       this.dom.style.backgroundPosition = '-113px -10px';
     }
-    // 设置小鸟的 top 值
+    // 設置小鳥的 top 值
     this.dom.style.top = this.top + 'px';
   },
-  // 设置小鸟的 top 值
+  // 設置小鳥的 top 值
   setTop(newTop){
     if (newTop < 0){
       newTop = 0;
@@ -148,17 +141,17 @@ bird = {
     this.speed = -0.1;
   }
 }
-// 让小鸟不停的扇动翅膀的计时器
+// 讓小鳥不停的扇動翅膀的計時器
 bird.wingTimer = getTimer(100,bird,function(){
-  // 这里面主要要做的事儿：修改 wingIndex，然后调用 show
+  // 這裡面主要要做的事兒：修改 wingIndex，然後調用 show
   this.wingIndex = (this.wingIndex + 1) % 3;
   this.show();
 });
 bird.dropTimer = getTimer(16,bird,function(){
-  // 主要要做的事儿：改变高度，然后调用 setTop 方法以及 show 方法
-  // 小鸟做的是匀加速运动
+// 主要要做的事兒：改變高度，然後調用 setTop 方法以及 show 方法
+  // 小鳥做的是勻加速運動
   // s = vt + 1/2 * a * t * t
-  // 如何获取匀加速的末速度：v = v0 + at
+  // 如何獲取勻加速的末速度：v = v0 + at
   let s = this.speed * 16 + 0.5 * this.a * 16 * 16;
   this.speed = this.speed + this.a * 16;
   this.setTop(this.top + s);
@@ -171,16 +164,16 @@ pipes = {
   getRandom : function(min,max){
     return Math.floor(Math.random() * (max - min) + min);
   },
-  all : [],  // 用于存放所有的柱子
-  // 创建柱子的方法
+  all : [],  // 用於存放所有的柱子
+  // 創建柱子的方法
   createPipe() {
     let minHeight = 60, // 柱子最小的高度
-      gap = 150; // 中间的间隙
+      gap = 150; // 中間的間隙
       maxHeight = 488 - gap - minHeight;
-    // 接下来确定一组柱子的高度
+    // 接下來確定一組柱子的高度
     let h1 = this.getRandom(minHeight,maxHeight),
       h2 = 488 - gap - h1;
-    // 接下来根据这两个高度来创建柱子
+    // 接下來根據這兩個高度來創建柱子
     // 上面的柱子
     let div1 = document.createElement("div");
     div1.className = "pipeup";
@@ -209,16 +202,16 @@ pipes = {
     });
   },
 }
-// 创建柱子
+// 創建柱子
 pipes.produceTimer = getTimer(2500,pipes,function(){
   this.createPipe();
 })
 
-// 移动柱子
+// 移動柱子
 pipes.moveTimer = getTimer(30,pipes,function(){
-  // 因为要移动所有的柱子   && 对游戏进行积分
+  // 因為要移動所有的柱子 && 對遊戲進行積分
   for(let i=0;i<this.all.length;i++){
-    let p = this.all[i]; // 得到当前的柱子
+    let p = this.all[i]; // 得到當前的柱子
     p.left -= 2;
     if (p.left < -p.width){
       p.dom.remove();
@@ -227,12 +220,12 @@ pipes.moveTimer = getTimer(30,pipes,function(){
     } else {
       p.dom.style.left = p.left + 'px';
     }
-    //判断柱子是否过了小鸟，若过了则说明小鸟过了一根柱子
+    // 判斷柱子是否過了小鳥，若過了則說明小鳥過了一根柱子
     if (p.left<=(bird.left-pipes.width)){
       console.log("+1"); 
     }
   }
-  game.gameOver(); // 每次柱子移动后，都需要判断游戏是否结束
+  game.gameOver(); // 每次柱子移動後，都需要判斷遊戲是否結束
 });
 
 document.documentElement.onkeydown = function(e){
@@ -240,7 +233,7 @@ document.documentElement.onkeydown = function(e){
     bird.jump();
   }
   if (e.key === 'Enter'){
-    // 按下回车键后，有三种状态（游戏运行中，游戏暂停中，游戏已结束）
+    // 按下Enter後，有三種狀態（遊戲運行中，遊戲暫停中，遊戲已結束）
     if (game.isGmaeOver){
       location.reload();
     }
